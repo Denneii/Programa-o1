cee6b493ead0c899375c9eff12f9ae1dc4649fcd
Dennis <106123685+Denneii@users.noreply.github.com>
2025-05-21 19:44:55 -0300

    Add files via upload

---
+++ programacao-1-app/programacao-1-app/src/main/java/com/dennis/programacao_1_app/Programacao1AppApplication.java
@@ -0,0 +1,13 @@
+package com.dennis.programacao_1_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Programacao1AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Programacao1AppApplication.class, args);
+	}
+
+}
---
+++ programacao-1-app/programacao-1-app/src/test/java/com/dennis/programacao_1_app/Programacao1AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dennis.programacao_1_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Programacao1AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
